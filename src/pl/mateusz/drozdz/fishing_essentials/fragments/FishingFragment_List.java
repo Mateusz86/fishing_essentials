eca65104c23aa011baa57f7cbaed3446465c7cdf
Pawel <westcpm13@wp.pl>
2014-03-06 17:48:44 +0100

    fix try catch change my_record_layout and did MyExpenditionAdapter

--- src/pl/mateusz/drozdz/fishing_essentials/fragments/FishingFragment_List.java
+++ src/pl/mateusz/drozdz/fishing_essentials/fragments/FishingFragment_List.java
@@ -1,8 +1,15 @@
 package pl.mateusz.drozdz.fishing_essentials.fragments;
 
+import java.util.List;
+
 import pl.mateusz.drozdz.fishing_essentials.R;
+import pl.mateusz.drozdz.fishing_essentials.core.DataBase;
+import pl.mateusz.drozdz.fishing_essentials.dao.Fishing;
+import pl.mateusz.drozdz.fishing_essentials.dao.FishingDao;
+import pl.mateusz.drozdz.fishing_essentials.list_adapter.MyExpeditionAdapter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +36,11 @@ public class FishingFragment_List extends Fragment {
 			Button show_form = (Button) view.findViewById(R.id.show_form);
 			ListView myExpenditionList = (ListView) view.findViewById(R.id.mojeWyprawyList);
 			
+			FishingDao fishingDao = DataBase.getInstance(getActivity()).getDaoSession().getFishingDao();			
+			final List<Fishing> fishes = fishingDao.queryBuilder().list();
+			Log.d("size fishes",fishes.size()+"");
+			myExpenditionList.setAdapter(new MyExpeditionAdapter(getActivity(), fishes));
+
 			
 			show_form.setOnClickListener(new View.OnClickListener() {
 
