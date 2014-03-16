d76b02f6391a6af1f6343252204d8ff4b609376b
Pawel <westcpm13@wp.pl>
2014-03-16 20:35:48 +0100

    google map

--- src/pl/mateusz/drozdz/fishing_essentials/FishingActivity.java
+++ src/pl/mateusz/drozdz/fishing_essentials/FishingActivity.java
@@ -1,14 +1,18 @@
 package pl.mateusz.drozdz.fishing_essentials;
 
 
+import com.google.android.gms.maps.MapFragment;
+
 import pl.mateusz.drozdz.fishing_essentials.fragments.FishingFragment_Form;
 import pl.mateusz.drozdz.fishing_essentials.fragments.FishingFragment_List;
 import pl.mateusz.drozdz.fishing_essentials.fragments.FishingFragment_List.ChangeFragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-public class FishingActivity extends FragmentActivity implements ChangeFragment {
+public class FishingActivity extends FragmentActivity  implements ChangeFragment,FishingFragment_Form.ChangeFragmenOnFragmentMap {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,9 +30,20 @@ public class FishingActivity extends FragmentActivity implements ChangeFragment 
 	@Override
 	public void changeFragment() {
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-	    FishingFragment_Form fishingFragment = new FishingFragment_Form();    	 
+	    FishingFragment_Form fishingFragment = new FishingFragment_Form(); 
+	    fishingFragment.setChangeFragmentOnFragmentMap(this);
 	    ft.replace(R.id.fragmentContainerFishing, fishingFragment);
 	    ft.addToBackStack(null);
 	    ft.commit();
+	}
+
+	@Override
+	public void changeFragmentOnFragmentMap() {
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.replace(R.id.fragmentContainerFishing, new pl.mateusz.drozdz.fishing_essentials.fragments.MapFragment());
+	    ft.addToBackStack(null);
+		ft.commit();
+
+
 	}
 }
